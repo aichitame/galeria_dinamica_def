2abbeb1aef2fe8e40b3f739fffa4d8fa5f2a70f4
aichitame <159391105+aichitame@users.noreply.github.com>
2025-08-28 21:39:10 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const productos = 
+[
+    {name: "ASUS TUF Gaming A16 FA506NCR-HN006 AMD Ryzen 7", price: 599, stars: 4.5, reviews: 510, seller: 'PcComponentes', img:"https://www.pccomponentes.com/portatil-asus-tuf-gaming-a15-fa506ncr-hn006-amd-ryzen-7-7435hs-16gb-512gb-ssd-rtx-3050-156"},
+
+    {name: "XFX SWIFT AMD Radeon RX 6600", price: 599, stars: 4.6, reviews: 119, seller: 'PcComponentes', img:"https://www.pccomponentes.com/tarjeta-grafica-msi-geforce-rtx-5070-ventus-2x-oc-12gb-gddr7-reflex-2-rtx-ai-dlss4"},
+
+    {name:"PcCom Ready V2 AMD Ryzen 7 5800X / 32GB / 1TB SSD / RTX 5060 Ti 16GB", price: 1199, stars:4.7, reviews: 1459, seller: 'PcComponentes', img:"https://www.pccomponentes.com/ordenador-sobremesa-pccom-ready-v2-amd-ryzen-7-5800x-32gb-1tb-ssd-rtx-5060-ti-16gb"},
+
+    {name:"AOC Q27G4XF 27'' LED Fast IPS QHD 180Hz", price: 170, stars: 4.6, reviews: 6053, seller: 'PcComponentes', img: "https://www.pccomponentes.com/monitor-aoc-q27g4xf-27-led-fast-ips-qhd-180hz",},
+
+    {name:"WD Black SN770 2TB Disco SSD 5150MB/S NVMe PCIe 4.0 M.2 Gen4 16GT/s", price: 118.99, stars: 4.5, reviews: 1872, seller: 'PcComponentes', img:"https://www.pccomponentes.com/disco-duro-wd-black-sn770-2tb-disco-ssd-5150mb-s-nvme-pcie-40-m2-gen4-16gt-s"},
+
+    {name:"Tempest Shake Silla Gaming con Reposapiés y Masaje Gris", price: 219, stars: 5, reviews: 135, seller: 'PcComponentes', img:"https://www.pccomponentes.com/tempest-shake-silla-gaming-con-reposapies-y-masaje-gris",},
+
+    {name:"Forgeon Captain RGB Auriculares con Micrófono Inalámbricos Gaming Negros", price: 59.99, stars: 4.5, reviews: 465, seller: 'PcComponentes', img:"https://www.pccomponentes.com/forgeon-captain-rgb-auriculares-con-microfono-inalambricos-gaming-negros",},
+
+    {name:"Samsung QE50Q60DAUXXH 43'' QLED UltraHD 4K Quantum HDR", price: 319, stars: 4.5, reviews: 345, seller: 'PcComponentes', img:"https://www.pccomponentes.com/tv-samsung-qe43q60dauxxh-43-qled-ultrahd-4k-quantum-hdr",},
+
+    {name:"Xiaomi Redmi Note 14 Pro 4G 8/256GB Morado Aurora Libre", price: 219, stars: 4.6, reviews: 406, seller: 'PcComponentes', img:"https://www.pccomponentes.com/xiaomi-redmi-note-14-pro-4g-8-256gb-morado-aurora-libre",},
+
+    {name:"Original AirFeel 3000W Aire Acondicionado Portátil con Bomba de Calor 3000 Frigorías", price: 309.90, stars: 4.5, reviews: 213, seller: 'PcComponentes', img:"https://www.pccomponentes.com/origial-airfeel-3000w-aire-acondicionado-portatil-bomba-de-calor-3000-frigorias"},
+    
+
+    
+]
+
